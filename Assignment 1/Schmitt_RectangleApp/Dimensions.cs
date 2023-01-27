b47623c0960b04cdc31eb721b52b73f33d833f11
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-01-27 10:40:27 -0600

    Complete Assignment 1

---
+++ Assignment 1/Schmitt_RectangleApp/Dimensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace RectangleApp
+{
+    /// <summary>
+    /// This class is responsible for calculating the things
+    /// </summary>
+    internal class Dimensions
+    {
+        #region Fields
+        decimal width;
+        decimal height;
+        #endregion Fields
+
+        /// <summary>
+        /// Empty Constructor (Unused in this assignment)
+        /// </summary>
+        internal Dimensions() { }
+
+        #region Getters
+
+        /// <summary>
+        /// Gets the height of this object
+        /// </summary>
+        /// <returns>The height field for this object</returns>
+        internal decimal GetHeight()
+        {
+            return height;
+        }
+
+        /// <summary>
+        /// Gets the width of this object
+        /// </summary>
+        /// <returns>The width field for this object</returns>
+        internal decimal GetWidth()
+        {
+            return width;
+        }
+        #endregion Getters
+
+        #region Setters
+        /// <summary>
+        /// Set the height of the rectangle from user's input
+        /// </summary>
+        internal void InputHeight()
+        {
+            Console.Write("Please enter the height of the rectangle: ");
+            height = Convert.ToDecimal(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Set the width of the rectangle from the user's input
+        /// </summary>
+        internal void InputWidth()
+        {
+            Console.Write("Please enter the width of the rectangle: ");
+            width = Convert.ToDecimal(Console.ReadLine());
+        }
+        #endregion Setters
+
+        #region Helper Methods
+        
+        /// <summary>
+        /// Calculates the area of the rectangle
+        /// </summary>
+        /// <param name="width">The width of the rectangle</param>
+        /// <param name="height">The height of the rectangle</param>
+        /// <returns>A decimal representing the area of the rectangle</returns>
+        internal static decimal CalculateArea(decimal width, decimal height)
+        {
+            return width * height;
+        }
+
+        /// <summary>
+        /// Calculates the permieter of a rectangle
+        /// </summary>
+        /// <param name="width">The width of the rectangle</param>
+        /// <param name="height">The height of the rectangle</param>
+        /// <returns>A decimal representing the permimeter of the rectangle</returns>
+        internal static decimal CalculatePerimeter(decimal width, decimal height)
+        {
+            return (width * 2) + (height * 2);
+        }
+        #endregion Helper Methods
+    }
+}
---
+++ Assignment 1/Schmitt_RectangleApp/Schmitt_RectangleApp.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace RectangleApp
+{
+    /// <summary>
+    /// This will be the main class for our Rectangle App
+    /// </summary>
+    internal static class Schmitt_RectangleApp
+    {
+        /// <summary>
+        /// The Main method where our code will start executing
+        /// </summary>
+        /// <param name="args">Represents a collection of strings that can be typed in the console directly</param>
+        internal static void Main(string[] args)
+        {
+            AssignmentInfo("Rectangle Assignment");
+            Directions();
+
+            Dimensions rectangle = new Dimensions();
+
+            rectangle.InputHeight();
+            rectangle.InputWidth();
+
+            decimal height = rectangle.GetHeight();
+            decimal width = rectangle.GetWidth();
+
+            decimal area = Dimensions.CalculateArea(height, width);
+            decimal permiter = Dimensions.CalculatePerimeter(height, width);
+
+            Console.WriteLine("\n\nThe area of the rectangle is " + area);
+            Console.WriteLine("The permimter of the rectangle is " + permiter);
+
+            Console.WriteLine("\nPress any key to close this window...");
+            Console.ReadKey();
+        }
+
+        internal static void AssignmentInfo(string assignment)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("*************************************\n\n");
+            sb.Append("Name:\t\tNathan Schmitt\n");
+            sb.Append("Course:\t\tITDEV-115\n");
+            sb.Append("Instructor:\tJ. Christie\n");
+            sb.Append("Assignment\t");
+            sb.Append(assignment);
+            sb.Append("\nDate:\t\t");
+            sb.Append(DateTime.Now);
+            sb.Append("\n\n*************************************\n");
+
+            Console.WriteLine(sb);
+            
+        }
+
+        internal static void Directions()
+        {
+            Console.WriteLine("This program will calculate the area and permiter of a rectangle");
+            Console.WriteLine("All you need to do is enter the rectangle's height and width\n");
+        }
+    }
+}
